c74e0a2de784726e5376993299d7b5826e6d36ea
hzdracom <gaofei@qwertyuio.local>
2015-10-30 10:50:27 +0800

    d

---
+++ name/name/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  name
+//
+//  Created by hzdracom on 15/10/30.
+//  Copyright © 2015年 hzdracom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ name/nameTests/nameTests.m
@@ -0,0 +1,39 @@
+//
+//  nameTests.m
+//  nameTests
+//
+//  Created by hzdracom on 15/10/30.
+//  Copyright © 2015年 hzdracom. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface nameTests : XCTestCase
+
+@end
+
+@implementation nameTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
