f7db0554ac1d20b141e1fbddeec63e85527b30f0
hzdracom <gaofei@qwertyuio.local>
2015-10-30 11:02:48 +0800

    jun

---
+++ jun/jun/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  jun
+//
+//  Created by hzdracom on 15/10/30.
+//  Copyright © 2015年 hzdracom. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"hello  word");
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
